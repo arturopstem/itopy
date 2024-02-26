a43f93e6dbf96c8c2919b20c5cf810b97c8b9e15
arturop <67555134+arturopstem@users.noreply.github.com>
2024-02-26 14:34:46 -0500

    Working with Variables

---
+++ intro/working-with-variables.py
@@ -0,0 +1,16 @@
+price = 150
+print(price)
+
+budget = 20
+print(budget + 10)
+
+price = 5
+amount = 3
+print(price * amount)
+
+score = 7 + 8
+print(score)
+
+points = 25
+points = 45
+print(points)
