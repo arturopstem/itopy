4e55bd2f50eeeccf1d638dc954b93e82323aeaa4
arturop <67555134+arturopstem@users.noreply.github.com>
2024-02-27 11:45:31 -0500

    Indexing

---
+++ intro/indexing.py
@@ -0,0 +1,12 @@
+last_calls = ["Mom", "Dave", "Lawyer"]
+print(last_calls[0])
+print(last_calls[1])
+
+animals = ["cat", "dog", "bird"]
+print(animals[2])
+
+nums = [8, 6, 19]
+nums[0] = 1
+nums[1] = 2
+nums[2] = 3
+print(nums)
