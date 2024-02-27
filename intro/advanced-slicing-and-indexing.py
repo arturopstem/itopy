177fa88b3017b24bf5462f44fb81166b5b180309
arturop <67555134+arturopstem@users.noreply.github.com>
2024-02-27 16:48:49 -0500

    Advanced Slicing and Indexing

---
+++ intro/advanced-slicing-and-indexing.py
@@ -0,0 +1,11 @@
+animals = ["cat", "dog", "bird", "cow"]
+print(animals[-1])
+
+vehicle = 'motorbike'
+print(vehicle[-1])
+
+c = ['$', 'ç', '€','ŧ']
+print(c[-2:])
+print(c[0:-2])
+c[:2] = ['Ŧ','ß']
+print(c)
