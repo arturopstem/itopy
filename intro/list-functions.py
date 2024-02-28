43f12fbaea0db4102340ad2cc408edf370f9d1e6
arturop <67555134+arturopstem@users.noreply.github.com>
2024-02-27 20:41:58 -0500

    List Functions

---
+++ intro/list-functions.py
@@ -0,0 +1,13 @@
+movie = "Avatar"
+print(len(movie))
+
+songs = ["Yesterday", "Hello", "Believer"]
+songs.append("Imagine")
+print(songs)
+
+items = ["book", "pen", "pencil"]
+items.insert(2, "marker")
+print(items)
+
+items.pop(1)
+print(items)
