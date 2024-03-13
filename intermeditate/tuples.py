cbd0a4178ff1b7daca7072716d41c50944927219
arturop <67555134+arturopstem@users.noreply.github.com>
2024-03-12 19:56:18 -0500

    Tuples

---
+++ intermeditate/tuples.py
@@ -0,0 +1,16 @@
+# string
+name = "Mery"
+# list
+cities = ["Madrid", "New York", "Paris"]
+print(cities)
+print(cities[1])
+
+colors = ["Yellow", "Red", "Blue"]
+colors[1] = "Orange"
+colors.append("Black")
+print(colors)
+
+# tuple
+coordinates = (48.8584, 2.2945)
+print(coordinates)
+print(coordinates[1])
