e1e9146521b7de97b629b4a5b7a060eb8ff3bf23
arturop <67555134+arturopstem@users.noreply.github.com>
2024-03-12 21:48:00 -0500

    Dictionaries

---
+++ intermeditate/dictionaries.py
@@ -0,0 +1,28 @@
+product = {
+  "name": "pen",
+  "color": "red",
+  "price": 79
+}
+print(product)
+
+car = {
+  "brand": "Audi",
+  "model": "Q5",
+  "model": "A5"
+}
+print(car)
+
+print(car["brand"])
+
+contact = {
+  "name": "John",
+  "company": "Google"
+}
+info_keys = contact.keys()
+info_values = contact.values()
+
+print(info_keys)
+print(info_values)
+
+info = car.items()
+print(info)
