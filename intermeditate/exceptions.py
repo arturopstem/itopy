8a132dd85a00e5e0ff1914e1f27c30f7b975b7b8
arturop <67555134+arturopstem@users.noreply.github.com>
2024-03-13 19:56:20 -0500

    Exceptions

---
+++ intermeditate/exceptions.py
@@ -0,0 +1,19 @@
+# code stops execution at line age -> 29
+# salary = 50000
+# role = Analyst
+# age -> 29
+# print(salary)
+
+# exception
+# name = "Bob"
+# name[0] = "R"
+# print(name)
+
+# NameError exception
+name = "Anna"
+print(surname)
+
+# SyntaxError exception
+score = 85
+if score >= 80
+  print("Passed")
